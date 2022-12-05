98aad1b575a6d212018f783e6b9a95df31084711
David Murdoch <dsmurdoch357@gmail.com>
2022-12-05 12:19:10 +0000

    Ran react codemod for imports

--- src/components/snackbar.tsx
+++ src/components/snackbar.tsx
@@ -1,10 +1,4 @@
-import React, {
-  createContext,
-  ReactNode,
-  useContext,
-  useEffect,
-  useState,
-} from "react";
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
 import tw, { styled } from "twin.macro";
 
 import "twin.macro";
